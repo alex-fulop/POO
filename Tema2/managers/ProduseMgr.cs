b2378222824861ea7f5beb713c5e0ec8f343b451
fulop <fulopalex2000@gmail.com>
2021-04-02 11:38:14 +0300

    Lab5 implementation - part 1

--- Tema2/managers/ProduseMgr.cs
+++ Tema2/managers/ProduseMgr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Xml;
 
 namespace Tema2
 {
@@ -26,6 +27,24 @@ namespace Tema2
                 Produs produs = (Produs) CitireProdus();
                 if (produs.CompareWith(Elemente)) Elemente.Add(produs);
                 nrProduse--;
+            }
+        }
+
+        static void CitireProduse()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("../Produse.xml");
+            XmlNodeList lista_noduri = doc.SelectNodes("/produse/Produs");
+            foreach (XmlNode nod in lista_noduri)
+            {
+                string nume = nod["Nume"]?.InnerText;
+                string codIntern = nod["CodIntern"]?.InnerText;
+                string producator = nod["Producator"]?.InnerText;
+                int pret = int.Parse(nod["Pret"]?.InnerText ?? string.Empty);
+                string categorie = nod["Categorie"]?.InnerText;
+                // int id = Elemente.Count + 1;
+                // Produs produs = new Produs(id, nume, codIntern, producator, pret, categorie);
+                // if (produs.CompareWith(Elemente)) Elemente.Add(
             }
         }
 
